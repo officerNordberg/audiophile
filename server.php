bce0b8d86e3f2c30dcbeb1034d76d1971b4d5f6c
officerNordberg <drew.michael.thompson@gmail.com>
2013-01-28 23:59:36 -0600

    playlists retrieved

--- server.php
+++ server.php
@@ -1,9 +1,16 @@
 <?php
 if ($_GET['address'])
 {
-       $out = exec("connect.py", [""] 
-
-        if ($_GET['control'] == "n") file_put_contents("msg", "Skipped");
+       $ret = "";
+       $out = `./connect.py audiophile`;
+       $list = explode("\n", $out);
+       #echo $_GET['address'];
+       #echo $ret; 
+       #$ret = shell_exec("python /var/www/daap/connect.py '" + $_GET['address'] + "'");
+       foreach($list as $playlist){
+           $ret .= "<h1>$playlist</h1>";
+       }
+       echo $ret;
 }
 
  ?>
